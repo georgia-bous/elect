d54ebe509772633279a58bfec7e796844b019310
Georgia Bousboukea <georgiamp208@gmail.com>
2023-01-26 22:40:03 +0200

    telos ta logins, problimataki me scroll stis 2 selides

---
+++ elect_logins/lib/home_screen.dart
@@ -0,0 +1,158 @@
+import 'package:flutter/material.dart';
+import '/sign_inScreen.dart';
+import '/log_inScreen.dart';
+//import 'package:google_fonts/google_fonts.dart';
+
+class Homescreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    double baseWidth = 600;
+    double baseHeight = 800;
+    double fem = MediaQuery.of(context).size.width / baseWidth;
+    double femm = MediaQuery.of(context).size.height / baseHeight;
+    double ffem = fem * 0.97;
+    return Scaffold(
+        body: Container(
+      padding: EdgeInsets.fromLTRB(25 * fem, 120 * femm, 20 * fem, 10 * femm),
+      color: Color(0xff1c1b1f),
+      child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: <
+          Widget>[
+        Container(
+            margin:
+                EdgeInsets.fromLTRB(140 * fem, 30 * femm, 52 * fem, 50 * femm),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  width: 113.88 * fem,
+                  height: 82.39 * fem,
+                  child: Image.asset(
+                    'assets/gemlogo-1.png',
+                  ),
+                ),
+                Container(
+                  width: 201.96 * fem,
+                  height: 77.42 * fem,
+                  child: Image.asset(
+                    'assets/electlogo2.png',
+                  ),
+                ),
+              ],
+            )),
+        Container(
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+              Center(
+                child: Container(
+                  margin: EdgeInsets.fromLTRB(
+                      70 * fem, 70 * femm, 70 * fem, 60 * femm),
+                  child: Text(
+                    'Welcome!',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontFamily: 'Rubik',
+                        fontSize: 45 * ffem,
+                        fontWeight: FontWeight.w400,
+                        color: Color(0xffffffff)),
+                  ),
+                ),
+              ),
+              Center(
+                child: Container(
+                  margin: EdgeInsets.fromLTRB(
+                      0 * fem, 0 * femm, 0 * fem, 15 * femm),
+                  child: Text(
+                    'Getting started!',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontFamily: 'Rubik',
+                      fontSize: 22 * ffem,
+                      fontWeight: FontWeight.w400,
+                      color: Color(0xffffffff),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(
+                    68 * fem, 0 * femm, 60 * fem, 55 * femm),
+                child: TextButton(
+                  onPressed: () => {
+                    Navigator.of(context)
+                        .push(MaterialPageRoute(builder: (context) => SignIn()))
+                  },
+                  child: Container(
+                    height: 60 * fem,
+                    decoration: BoxDecoration(
+                      color: Color(0xff6750a4),
+                      borderRadius: BorderRadius.circular(100 * fem),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Sign In',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontFamily: 'Rubik',
+                          fontSize: 25 * ffem,
+                          fontWeight: FontWeight.w500,
+                          letterSpacing: 0.1 * fem,
+                          color: Color(0xffffffff),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Center(
+                child: Container(
+                  margin: EdgeInsets.fromLTRB(
+                      0 * fem, 0 * femm, 0 * fem, 15 * femm),
+                  child: Text(
+                    'Already have an account?',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontFamily: 'Rubik',
+                      fontSize: 22 * ffem,
+                      fontWeight: FontWeight.w400,
+                      color: Color(0xffffffff),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                margin:
+                    EdgeInsets.fromLTRB(68 * fem, 0 * femm, 68 * fem, 0 * femm),
+                child: TextButton(
+                  onPressed: () => {
+                    Navigator.pop(context),
+                    Navigator.of(context)
+                        .push(MaterialPageRoute(builder: (context) => LogIn()))
+                  },
+                  child: Container(
+                    height: 60 * fem,
+                    decoration: BoxDecoration(
+                      color: Color(0xff6750a4),
+                      borderRadius: BorderRadius.circular(100 * fem),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Log In',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontFamily: 'Rubik',
+                          fontSize: 25 * ffem,
+                          fontWeight: FontWeight.w500,
+                          letterSpacing: 0.1 * fem,
+                          color: Color(0xffffffff),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ]))
+      ]),
+    ));
+  }
+}
---
+++ elect_logins/lib/sign_inScreen.dart
@@ -0,0 +1,214 @@
+import 'package:flutter/material.dart';
+import '/log_inScreen.dart';
+
+class SignIn extends StatefulWidget {
+  const SignIn({Key? key}) : super(key: key);
+
+  @override
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  @override
+  Widget build(BuildContext context) {
+    double baseWidth = 600;
+    double baseHeight = 800;
+    double fem = MediaQuery.of(context).size.width / baseWidth;
+    double femm = MediaQuery.of(context).size.height / baseHeight;
+    double ffem = fem * 0.97;
+    return Scaffold(
+        body: SingleChildScrollView(
+            child: Container(
+                padding: EdgeInsets.fromLTRB(
+                    25 * fem, 120 * femm, 20 * fem, 10 * femm),
+                color: Color(0xff1c1b1f),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                        margin: EdgeInsets.fromLTRB(
+                            140 * fem, 30 * femm, 52 * fem, 50 * femm),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Container(
+                              width: 113.88 * fem,
+                              height: 82.39 * fem,
+                              child: Image.asset(
+                                'assets/gemlogo-1.png',
+                              ),
+                            ),
+                            Container(
+                              width: 201.96 * fem,
+                              height: 77.42 * fem,
+                              child: Image.asset(
+                                'assets/electlogo2.png',
+                              ),
+                            ),
+                          ],
+                        )),
+                    Container(
+                        child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Center(
+                          child: Container(
+                            margin: EdgeInsets.fromLTRB(
+                                20 * fem, 70 * femm, 20 * fem, 40 * femm),
+                            child: Text(
+                              'Let\'s create your account!',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  fontFamily: 'Rubik',
+                                  fontSize: 30 * ffem,
+                                  fontWeight: FontWeight.w400,
+                                  color: Color(0xffffffff)),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          width: double.infinity,
+                          height: 75 * fem,
+                          margin: EdgeInsets.fromLTRB(
+                              30 * fem, 0 * femm, 10 * fem, 30 * femm),
+                          padding: EdgeInsets.fromLTRB(
+                              20 * fem, 1 * femm, 20 * fem, 3 * femm),
+                          decoration: BoxDecoration(
+                            color: Color(0xff25232a),
+                            borderRadius: BorderRadius.circular(50 * fem),
+                          ),
+                          child: TextField(
+                              decoration: InputDecoration(
+                                hintText: 'Enter your username',
+                                hintStyle: TextStyle(
+                                  color: Colors.white54,
+                                  fontFamily: 'Rubik',
+                                  fontSize: 20 * ffem,
+                                  fontWeight: FontWeight.w400,
+                                  letterSpacing: 0.5 * fem,
+                                ),
+                                border: InputBorder.none,
+                              ),
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontFamily: 'Rubik',
+                                  fontSize: 24 * ffem,
+                                  fontWeight: FontWeight.normal,
+                                  letterSpacing: 0.5 * fem)),
+                        ),
+                        Container(
+                          margin: EdgeInsets.fromLTRB(
+                              30 * fem, 0 * femm, 10 * fem, 30 * femm),
+                          width: double.infinity,
+                          height: 75 * fem,
+                          padding: EdgeInsets.fromLTRB(
+                              20 * fem, 1 * femm, 20 * fem, 3 * femm),
+                          decoration: BoxDecoration(
+                            color: Color(0xff25232a),
+                            borderRadius: BorderRadius.circular(50 * fem),
+                          ),
+                          child: TextField(
+                              decoration: InputDecoration(
+                                hintText: 'Enter your email',
+                                hintStyle: TextStyle(
+                                  color: Colors.white54,
+                                  fontFamily: 'Rubik',
+                                  fontSize: 20 * ffem,
+                                  fontWeight: FontWeight.w400,
+                                  letterSpacing: 0.5 * fem,
+                                ),
+                                border: InputBorder.none,
+                              ),
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontFamily: 'Rubik',
+                                  fontSize: 24 * ffem,
+                                  fontWeight: FontWeight.normal,
+                                  letterSpacing: 0.5 * fem)),
+                        ),
+                        Container(
+                          margin: EdgeInsets.fromLTRB(
+                              30 * fem, 0 * femm, 10 * fem, 30 * femm),
+                          width: double.infinity,
+                          height: 75 * fem,
+                          padding: EdgeInsets.fromLTRB(
+                              20 * fem, 1 * femm, 20 * fem, 3 * femm),
+                          decoration: BoxDecoration(
+                            color: Color(0xff25232a),
+                            borderRadius: BorderRadius.circular(50 * fem),
+                          ),
+                          child: TextField(
+                            obscureText: true,
+                            style: TextStyle(color: Colors.white),
+                            decoration: InputDecoration(
+                              hintText: 'Enter your password',
+                              hintStyle: TextStyle(
+                                color: Colors.white54,
+                                fontFamily: 'Rubik',
+                                fontSize: 20 * ffem,
+                                fontWeight: FontWeight.w400,
+                                letterSpacing: 0.5 * fem,
+                              ),
+                              border: InputBorder.none,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.fromLTRB(
+                              133 * fem, 0 * femm, 133 * fem, 10 * femm),
+                          child: TextButton(
+                            onPressed: () {},
+                            child: Container(
+                              height: 80 * fem,
+                              decoration: BoxDecoration(
+                                color: Color(0xff6750a4),
+                                borderRadius: BorderRadius.circular(100 * fem),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  'Register',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontFamily: 'Rubik',
+                                    fontSize: 27 * ffem,
+                                    fontWeight: FontWeight.bold,
+                                    letterSpacing: 0.1 * fem,
+                                    color: Color(0xffffffff),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.fromLTRB(
+                              100 * fem, 0 * femm, 100 * fem, 5 * femm),
+                          child: TextButton(
+                            onPressed: () => {
+                              Navigator.pop(context),
+                              Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (context) => LogIn()))
+                            },
+                            child: Container(
+                              child: Center(
+                                child: Text(
+                                  'Already have an account? Log in!',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontFamily: 'Rubik',
+                                    fontSize: 20 * ffem,
+                                    fontWeight: FontWeight.w200,
+                                    letterSpacing: 0.1 * fem,
+                                    color: Color(0xff6750a4),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ))
+                  ],
+                ))));
+  }
+}
